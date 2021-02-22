4b41b2a99cbbf119d09345a1cbedcc223b873b25
Lightmani <43240742+Lightmani@users.noreply.github.com>
2021-02-22 19:05:23 +0800

    Create V2_XTLS_Centos7.sh

---
+++ V2_XTLS_Centos7.sh
@@ -0,0 +1,145 @@
+#初始环境
+environment(){
+
+read -p " 请输入你的网址:" yoursite
+
+
+yum update -y
+yum install wget curl unzip tar policycoreutils-python  rng-tools vim firewalld cron -y
+
+
+
+#bbr
+
+
+
+}
+
+modify_port_UUID(){
+
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+
+    sed -i "/\"id\"/c \\\t  \"id\":\"${UUID}\"," /etc/xray/config.json
+
+
+}
+
+#Firewall
+firewall(){
+systemctl enable firewalld
+systemctl start firewalld
+firewall-cmd --permanent --zone=public --add-port=22222/tcp
+firewall-cmd --permanent --zone=public --add-port=80/tcp
+firewall-cmd --permanent --zone=public --add-port=443/tcp
+firewall-cmd --reload
+}
+
+
+ssha(){
+cd /root
+mkdir .ssh/
+read -p " 请输入你的KEY:" key
+echo $key >> /root/.ssh/authorized_keys
+echo "PasswordAuthentication no">>/etc/ssh/sshd_config
+echo "PubkeyAuthentication yes">>/etc/ssh/sshd_config
+echo "Port 22222">>/etc/ssh/sshd_config
+semanage port -a -t ssh_port_t -p tcp 22222
+
+systemctl restart sshd.service
+
+}
+
+
+#LNMP一键
+v2_nginx(){
+
+
+yum install -y curl vim wget unzip apt-transport-https lsb-release ca-certificates git gnupg2 netcat socat epel-release
+
+mkdir /etc/xray
+curl  https://get.acme.sh | sh
+~/.acme.sh/acme.sh --issue -d $yoursite --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $yoursite --fullchainpath /etc/xray/v2ray.crt --keypath /etc/xray/v2ray.key --ecc
+
+
+
+
+
+
+yum install -y nginx
+systemctl enable nginx
+
+cat >> /etc/nginx/sites-enabled/site.conf << EOF
+server {  
+        listen 80;
+        listen [::]:80;
+        root /var/www/site/; 
+        index index.php index.html;
+        server_name $yoursite; 
+
+
+      
+}
+EOF
+
+
+
+
+
+
+#rng
+echo "HRNGDEVICE=/dev/urandom">>/etc/default/rng-tools
+
+
+#V2ray
+wget -qO- get.docker.com | bash
+systemctl enable docker
+systemctl start docker
+
+mkdir /etc/xray
+cd /etc/xray
+rm config.json
+*****************************************
+wget https://github.com/Lightmani/Docker_NetTools/raw/master/config/V2_XTLS.config  -cO config.json
+modify_port_UUID
+docker pull teddysun/xray
+docker run -d --name v2ray --restart always --net host -v /etc/xray:/etc/xray teddysun/xray
+
+service nginx restart
+
+
+
+echo "*******************************************************************"
+
+echo -e "${Red} 用户id（UUID）：${Font} ${UUID}"
+}
+
+
+echo -e "1.Environment"
+echo -e "2.V2Ray+Nginx"
+echo -e "3.SSH"
+echo -e "4.Firewall"
+echo -e "5.All"
+read -p "Press:" menu_Num
+case "$menu_Num" in
+	1)
+	environment
+	;;
+	2)
+	v2_nginx
+	;;
+	3)
+	ssha
+	;;
+	4)
+	firewall
+	;;
+	5)
+	environment
+    firewall
+    v2_nginx
+	;;
+	*)
+	echo "Enter Right[1-5]:"
+	;;
+esac
