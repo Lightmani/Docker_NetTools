0830e7782b29946ab3169cff5ed6986a6361a5b9
Lightmani <43240742+Lightmani@users.noreply.github.com>
2021-04-16 17:14:33 +0800

    Update V2_XTLS_Centos7.sh

--- V2_XTLS_Centos7.sh
+++ V2_XTLS_Centos7.sh
@@ -12,7 +12,7 @@ rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml -y
 grub2-mkconfig -o /boot/grub2/grub.cfg
 sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=0/g' /etc/default/grub
-
+grub2-mkconfig -o /boot/grub2/grub.cfg
 
 #bbr
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
