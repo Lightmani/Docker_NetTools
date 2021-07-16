25b952e54b500cf3e7617634db63625999de35df
Lightmani <43240742+Lightmani@users.noreply.github.com>
2021-07-16 17:17:35 +0800

    Update Docker_V2_Xtls.sh

--- Docker_V2_Xtls.sh
+++ Docker_V2_Xtls.sh
@@ -70,7 +70,7 @@ curl  https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --installcert -d $yoursite --fullchainpath /etc/xray/v2ray.crt --keypath /etc/xray/v2ray.key --ecc
 
 (echo "59 23 * * * service nginx stop >> /dev/null 2>&1" ; crontab -l ) | crontab
-(echo "1 0 * * * service nginx start >> /dev/null 2>&1" ; crontab -l ) | crontab
+(echo "1 1 * * * service nginx start >> /dev/null 2>&1" ; crontab -l ) | crontab
 
 
 
