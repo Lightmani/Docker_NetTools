0f7fb4f9788d76b29f171bae8901fd43d90332e9
Lightmani <43240742+Lightmani@users.noreply.github.com>
2021-08-17 18:44:52 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,11 @@
 yum remove nginx -y
 apt remove nginx -y
 systemctl disable nginx
+rm -rf /etc/nginx
 
 acme.sh --uninstall
 
 docker stop v2ray
 docker rm v2ray
 docker rmi teddysun/xray
+rm -rf /etc/xray/
