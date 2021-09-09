13de770ea3782ca34b92069f73898d4f10be6990
Lightmani <43240742+Lightmani@users.noreply.github.com>
2021-09-09 09:14:54 +0800

    Update Docker_SS_Gost.sh

--- Docker_SS_Gost.sh
+++ Docker_SS_Gost.sh
@@ -9,7 +9,7 @@ apt install vim ufw curl wget unzip rng-tools -y
 
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
-echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf
+echo "net.core.default_qdisc = cake" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
 sysctl -p >/dev/null 2>&1
 
