2144f848565adeaf46fb3b0a11773cbd2a5b1391
Lightmani <43240742+Lightmani@users.noreply.github.com>
2022-05-20 22:10:29 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 service nginx stop
 
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh"
+chmod 755 /etc/v2ray/*
+cat /etc/v2ray/v2ray.crt /etc/v2ray/v2ray.key > /etc/v2ray/v2ray.pem
 
 service v2ray restart
 service haproxy restart
