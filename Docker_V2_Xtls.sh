e855940b570a7e42e20255828c55dc17dd68cbfe
Lightmani <43240742+Lightmani@users.noreply.github.com>
2022-05-21 23:19:13 +0800

    Update Docker_V2_Xtls.sh

--- Docker_V2_Xtls.sh
+++ Docker_V2_Xtls.sh
@@ -14,7 +14,7 @@ apt-get remove --purge nginx nginx-full nginx-common -y
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 cat >> /etc/sysctl.conf << EOF
-net.core.default_qdisc=fq_codel
+net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
 sysctl -p && sysctl --system
