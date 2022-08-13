96b8e299f267ab6b806e4922b21be2469d1659ea
Lightmani <43240742+Lightmani@users.noreply.github.com>
2022-08-13 12:49:42 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 service nginx stop
+service caddy stop
+
 
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh"
 chmod 755 /etc/v2ray/*
@@ -7,3 +9,5 @@ cat /etc/v2ray/v2ray.crt /etc/v2ray/v2ray.key > /etc/v2ray/v2ray.pem
 service v2ray restart
 service haproxy restart
 service nginx start
+service caddy start
+
