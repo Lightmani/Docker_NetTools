e3f3163b2d4be840fbcd31e825507fc8a0383a6c
Lightmani <43240742+Lightmani@users.noreply.github.com>
2022-08-13 12:45:24 +0800

    Update integrated_v2.sh

--- integrated_v2.sh
+++ integrated_v2.sh
@@ -231,8 +231,8 @@ clear
 echo "*******************************************************************"
 echo -e "${Red} 用户域名：${Font} ${yoursite}"
 echo -e "${Red} 用户id（UUID）：${Font} ${UUID}"
-echo -e "${Red} H2传输Path ：${Font} ${path}"
-echo -e "${Red} GRPC传输Path ：${Font} ${cdnspath}"
+echo -e "${Red} H2传输Path ：/speedtest"
+echo -e "${Red} GRPC传输Path ：/speedtest1"
 
 }
 
