2d92d05a5d8eef293d6385c6c4fb3345d26fd9ef
Lightmani <43240742+Lightmani@users.noreply.github.com>
2022-09-14 00:55:19 +0800

    Update integrated_v2.sh

--- integrated_v2.sh
+++ integrated_v2.sh
@@ -32,23 +32,7 @@ sed -i '/net.ipv4.udp_rmem_min/d' /etc/sysctl.conf
 sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
 
 cat >> /etc/sysctl.conf << EOF
-net.ipv4.tcp_no_metrics_save=1
-net.ipv4.tcp_ecn=0
-net.ipv4.tcp_frto=0
-net.ipv4.tcp_mtu_probing=0
-net.ipv4.tcp_rfc1337=1
-net.ipv4.tcp_sack=1
-net.ipv4.tcp_fack=1
-net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_adv_win_scale=1
-net.ipv4.tcp_moderate_rcvbuf=1
-net.ipv4.tcp_rmem=4096 32768 16777216
-net.ipv4.tcp_wmem=4096 32768 16777216
-net.core.rmem_max=16777216
-net.core.wmem_max=16777216
-net.ipv4.udp_rmem_min=8192
-net.ipv4.udp_wmem_min=8192
-net.core.default_qdisc=fq_codel
+net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 EOF
 sysctl -p && sysctl --system
